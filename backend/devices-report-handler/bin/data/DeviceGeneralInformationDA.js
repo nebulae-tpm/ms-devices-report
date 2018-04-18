379ea809733f15f625d0922e636aaf0dab250420
juansmolano <juansmolano@gmail.com>
2018-04-18 10:30:09 -0500

    work in progres

---
+++ backend/devices-report-handler/bin/data/DeviceGeneralInformationDA.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const mongoDB = require('./MongoDB')();
+const Rx = require('rxjs');
+const CollectionName = "DeviceGeneralInformation";
+
+class DeviceGeneralInformationDA {
+
+
+    /**
+     * gets DeviceGeneralInformation by device serial number
+     * @param {string} type 
+     */
+    static getDeviceGeneralInformation$(sn) {
+        const collection = mongoDB.db.collection(CollectionName);
+        return Rx.Observable.fromPromise(collection.findOne({ sn }));
+    }
+
+    /**
+     * Updates a device general informtion
+     * @param {String} sn Device serial number
+     * @param {[{key, value}]} properties Array of properties to update
+     * @param {number} aggregateVersion new aggregate version
+     * @param {number} aggregateVersionTimestamp new aggregate version timestamp
+     */
+    static updateDeviceGenearlInformation$(sn, properties, aggregateVersion, aggregateVersionTimestamp) {
+        const collection = mongoDB.db.collection(CollectionName);
+        const update = {
+            $set: {
+                aggregateVersion,
+                aggregateVersionTimestamp,
+                aggregateUpdatedTimestamp: Date.now()
+            }
+        };
+        properties.forEach(prop => {
+            update[$set][prop.key] = prop.value;
+        });
+
+        return Rx.Observable.fromPromise(
+            collection.updateOne(
+                { sn },
+                update,
+                { upsert: true }
+            ));
+    }
+
+}
+
+module.exports = DeviceGeneralInformationDA;
